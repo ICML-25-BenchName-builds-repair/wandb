8fefd85992dc07fd9c0d7e38ca269759671e4121
ubuntu <ubuntu@ip-10-0-1-48>
2025-01-29 13:39:01 +0000

    waleko__gemini-pro-1.5_b7d1__id_116

--- wandb/integration/ultralytics/bbox_utils.py
+++ wandb/integration/ultralytics/bbox_utils.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 import torch
 from ultralytics.engine.results import Results
 from ultralytics.models.yolo.detect import DetectionPredictor
+
 try:
     from ultralytics.yolo.utils import ops
 except ModuleNotFoundError:
@@ -35,7 +36,7 @@ def scale_bounding_box_to_original_image_shape(
 
 
 def get_ground_truth_bbox_annotations(
-    img_idx: int, image_path: str, batch: Dict, class_name_map: Dict = None
+    img_idx: int, image_path: str, batch: Dict, class_name_map: Optional[Dict] = None
 ) -> List[Dict[str, Any]]:
     """Get ground truth bounding box annotation data in the form required for `wandb.Image` overlay system."""
     indices = batch["batch_idx"] == img_idx
